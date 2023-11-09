39109ebbadffcdf215b05e16fc77d50945005978
jun1090 <abide0206@naver.com>
2023-11-09 18:15:15 +0900

    Feat-vue로 시작 로그인페이지 완, 회원가입페이지 구현중

---
+++ reference/App.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="black-bg" v-if="모달창열렸니 === true">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지 내용임</p>
+      <button @click="모달창열렸니 = false">닫기버튼임</button>
+    </div>
+  </div>
+
+  <div class="menu">
+    <a v-for="(a, i) in menus" :key="i">{{ a }}</a>
+  </div>
+
+  <div v-for="(room, i) in 원룸데이터" :key="i">
+    <img :src="room.image" class="room-img" />
+    <h4 @click="모달창열렸니 = true">{{ room.title }}</h4>
+    <P>{{ room.price }}만원</P>
+  </div>
+</template>
+
+<script>
+import roomData from './assets/oneroom.js';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      원룸데이터: roomData,
+      모달창열렸니: false,
+      역삼동신고수: 0,
+      천호동신고수: 0,
+      마포구신고수: 0,
+      price1: 60,
+      price2: 70,
+      스타일: 'color: blue',
+      menus: ['Home', 'Shop', 'About'],
+      product: ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    };
+  },
+  methods: {
+    increase(area) {
+      if (area === '역삼동') {
+        this.역삼동신고수 += 1;
+      } else if (area === '천호동') {
+        this.천호동신고수 += 1;
+      } else if (area === '마포구') {
+        this.마포구신고수 += 1;
+      }
+    },
+  },
+  components: {},
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  padding: 20px;
+  border-radius: 8px;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+</style>
---
+++ todolist-for-db/src/App.vue
@@ -0,0 +1,149 @@
+<template>
+  <!-- 시작화면_로그인화면 -->
+  <div id="startPage_login" v-if="startPage_state === true">
+    <h1>ToDoList</h1>
+    <div id="totalBox">
+      <h2><img src="./assets/logo2.png" alt="로고 이미지" /></h2>
+      <label for="username">아이디</label>
+      <input type="text" id="username" placeholder="아이디를 입력하세요" />
+      <label for="password">비밀번호</label>
+      <input
+        type="password"
+        id="password"
+        placeholder="비밀번호를 입력하세요"
+      />
+      <div class="button-box">
+        <button id="login-button">로그인</button>
+        <div class="lower-buttonBox">
+          <button class="signup-button">회원가입</button>
+          <button class="modify-button">회원정보 수정</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- 회원가입 화면 -->
+  <div id="startPage_login" v-if="signUpPage_state === true">
+    <h1>ToDoList</h1>
+    <div id="totalBox" class="signUpPage_box">
+      <h2>가입하기</h2>
+      <label for="username">아이디</label>
+      <input type="text" id="username" placeholder="아이디를 입력하세요" />
+      <label for="password">비밀번호</label>
+      <input
+        type="password"
+        id="password"
+        placeholder="비밀번호를 입력하세요"
+      />
+      <label for="nickname">닉네임</label>
+      <input type="text" id="nickname" placeholder="닉네임을 입력하세요" />
+      <button id="signup-button">회원가입</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      startPage_state: true,
+      signUpPage_state: false,
+    };
+  },
+  methods: {},
+  components: {},
+};
+</script>
+
+<style>
+/* 공통 */
+* {
+  box-sizing: border-box;
+}
+html,
+body {
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  font-family: Arial, sans-serif;
+  text-align: center;
+  background-color: #f4f4f4;
+}
+#totalBox {
+  width: 500px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  background-color: #fff;
+}
+/* 시작화면 페이지 */
+h1 {
+  background-color: #333;
+  color: #fff;
+  padding: 20px;
+  margin: 0;
+}
+h2 {
+  margin: 0;
+}
+img {
+  width: auto;
+  height: 90px;
+}
+
+label {
+  display: block;
+  text-align: left;
+  margin-top: 10px;
+  margin-bottom: 5px;
+}
+input {
+  width: 100%;
+  padding: 10px;
+  margin-top: 5px;
+  margin-bottom: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+button {
+  width: 100%;
+  padding: 15px;
+  background-color: #333;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  display: inline-block;
+  margin-bottom: 3px;
+}
+button:hover {
+  background-color: #555;
+}
+.button-box {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+#login-button {
+  margin-bottom: 20px;
+  margin-top: 15px;
+}
+.lower-buttonBox {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.signup-button,
+.modify-button {
+  width: 48%;
+}
+/* 회원가입 페이지 */
+.signUpPage_box {
+  height: 425px;
+}
+#signup-button {
+  margin-top: 20px;
+  padding: 20px;
+}
+</style>
