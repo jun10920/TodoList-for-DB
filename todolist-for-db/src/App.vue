b6635f1942161477f1b732d23ac890c006261a26
jun1090 <abide0206@naver.com>
2023-11-14 01:18:33 +0900

    컴포넌트 퍼블리싱 완료

--- todolist-for-db/src/App.vue
+++ todolist-for-db/src/App.vue
@@ -13,10 +13,19 @@
         placeholder="비밀번호를 입력하세요"
       />
       <div class="button-box">
-        <button id="login-button">로그인</button>
+        <button
+          @click="(todoListPage_state = true), (startPage_state = false)"
+          id="login-button"
+        >
+          로그인
+        </button>
         <div class="lower-buttonBox">
-          <button class="signup-button">회원가입</button>
-          <button class="modify-button">회원정보 수정</button>
+          <button
+            @click="(signUpPage_state = true), (startPage_state = false)"
+            class="signup-button"
+          >
+            회원가입
+          </button>
         </div>
       </div>
     </div>
@@ -36,7 +45,81 @@
       />
       <label for="nickname">닉네임</label>
       <input type="text" id="nickname" placeholder="닉네임을 입력하세요" />
-      <button id="signup-button">회원가입</button>
+      <button
+        @click="(signUpPage_state = false), (startPage_state = true)"
+        id="signup-button"
+      >
+        회원가입
+      </button>
+    </div>
+  </div>
+
+  <!-- 메인화면_투두리스트 화면 -->
+  <div id="todoListPage" v-if="todoListPage_state === true">
+    <h1>ToDoList</h1>
+    <div id="totalBox">
+      <div class="user-icon">
+        <img src="../src/assets/member icon.png" alt="Member Icon" style="width:
+        45px; height: 45px;" cursor: pointer @click="(todoListPage_state =
+        false), (personalInfo_state = true)" >
+        <!-- 이미지 크기 조절 -->
+      </div>
+      <div class="category">
+        <label for="category">카테고리:</label>
+        <input
+          class="inputCategory"
+          type="text"
+          id="inputCategory"
+          placeholder="카테고리를 입력하세요"
+        />
+        <button class="remove-category-button">-</button>
+        <button class="add-category-button">+</button>
+      </div>
+      <div class="task-list">
+        <span class="check-box">&#10003;</span>
+        <input
+          type="text"
+          class="task-input"
+          placeholder="할 일을 입력하세요"
+        />
+        <span class="expand-button">...</span>
+      </div>
+    </div>
+  </div>
+  <!-- 회원정보 수정 페이지 -->
+  <div id="personalInfo" v-if="personalInfo_state === true">
+    <h1>ToDoList</h1>
+    <div id="totalBox">
+      <h2>회원정보 수정</h2>
+      <label for="new-username">아이디 변경</label>
+      <input
+        type="text"
+        id="new-username"
+        placeholder="새로운 아이디를 입력하세요"
+      />
+      <label for="new-password">비밀번호 변경</label>
+      <input
+        type="password"
+        id="new-password"
+        placeholder="새로운 비밀번호를 입력하세요"
+      />
+      <label for="new-nickname">닉네임 변경</label>
+      <input
+        type="text"
+        id="new-nickname"
+        placeholder="새로운 닉네임을 입력하세요"
+      />
+
+      <div class="button-container">
+        <button
+          @click="(personalInfo_state = false), (todoListPage_state = true)"
+        >
+          뒤로가기
+        </button>
+        <button @click="(personalInfo_state = false), (startPage_state = true)">
+          로그아웃
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -48,6 +131,8 @@ export default {
     return {
       startPage_state: true,
       signUpPage_state: false,
+      todoListPage_state: false,
+      personalInfo_state: false,
     };
   },
   methods: {},
@@ -134,9 +219,8 @@ button:hover {
   flex-direction: row;
   justify-content: space-between;
 }
-.signup-button,
-.modify-button {
-  width: 48%;
+.signup-button {
+  width: 100%;
 }
 /* 회원가입 페이지 */
 .signUpPage_box {
@@ -145,5 +229,90 @@ button:hover {
 #signup-button {
   margin-top: 20px;
   padding: 20px;
+}
+/* 투두리스트 페이지 */
+/* 카테고리 */
+.user-icon {
+  font-size: 10px; /* 아이콘 크기를 조절 */
+  color: #555;
+  cursor: pointer;
+}
+.category,
+.task-list {
+  display: flex;
+  position: relative;
+  justify-content: center;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+  margin-top: 10px;
+  height: 55px;
+}
+.category {
+  align-items: center;
+}
+.category label {
+  margin: 0;
+  padding-right: 25px;
+}
+.inputCategory {
+  width: 280px;
+  margin: 0;
+}
+
+/* "+" 버튼 스타일 */
+.add-category-button,
+.remove-category-button {
+  background-color: white;
+  font-size: 25px;
+  color: black;
+  cursor: pointer;
+  width: 5px;
+  height: auto;
+  padding: 9px;
+  margin-left: 7px;
+}
+
+/* "-" 버튼 스타일 */
+.remove-category-button {
+  color: #f00;
+  margin-right: 2px;
+}
+
+/* 체크 표시 칸 스타일 */
+.check-box {
+  cursor: pointer;
+  font-size: 20px;
+  color: #555;
+  margin-right: 20px;
+}
+
+/* "..." 버튼 스타일 */
+.expand-button {
+  margin-left: 5px;
+  cursor: pointer;
+  font-size: 20px;
+  color: #555;
+}
+
+/* 할일 입력란 스타일 */
+.task-input {
+  margin: 0;
+  width: 80%;
+  margin-right: 10px;
+}
+
+/* 회원정보 수정 페이지 */
+.button-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+}
+.button-container button {
+  margin-top: 10px;
+  width: 220px;
+  height: 70px;
+  font-size: 25px;
 }
 </style>
