c43fc7b1ed838e2fd065c80f4e6caff7cfda5a50
jun1090 <abide0206@naver.com>
2023-11-16 01:49:16 +0900

    feat-axios 이용해서 서버 통해서 todo 추가하기/내일 할 것 모달창 생성해서 수정 삭제 버튼 만들기

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+
+const memos = [];
+
+app.use(bodyParser.json(), express.urlencoded({ extended: true }));
+
+app.get('/api/memos', (req, res) => {
+  res.send(memos);
+});
+
+app.post('/api/memos', (req, res) => {
+  // console.log(req.body);
+  memos.push(req.body.content);
+  res.send(memos);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- js/index.js
+++
@@ -1,3 +0,0 @@
-import App from './views/App.js';
-
-new App(document.querySelector('body'));
